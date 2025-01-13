1a6144833d6534d191a2120820830f6970c7255d
Arpita.s <arpita.sahoo637174@gail.com>
2025-01-13 22:17:54 +0530

    data passing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { DUMMY_USERS } from './dummy-user';
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'EasyTask';
   users = DUMMY_USERS;
-
+  selectedUser!: any;
   OnrcvUser(event: string) {
-    console.log(event);
+    this.selectedUser = this.users.find((u) => u.id === event);
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -20,7 +20,7 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
   @Input({ required: true }) id!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
